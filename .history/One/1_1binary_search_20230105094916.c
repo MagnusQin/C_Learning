1fc6630213ef0c50e37109288ebd152ecdb2ef88
MagnusQin <2541476269@qq.com>
2023-01-05 17:57:19 +0800

    1.5

---
+++ .history/One/1_1binary_search_20230105094916.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int binary_search(int a[], int k, int sz){
+    int left = 0;
+    int right = sz - 1;
+    while(left <= right){
+        int mid = (left + right) / 2;
+        if(a[mid] > k){
+            right = a[mid] -1;
+        }
+        else if (a[mid] < k){
+            left = a[mid] +1;
+        }
+        else
+            return mid;
+    }
+    return -1;
+}
+
+int main(){
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    int key = 5;
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int ret = binary_search(arr, key, size);
+    if(ret == -1){
+        printf("未找到！");
+    }
+    else
+        printf("找到了，其下标为：%d",ret);
+    return 0;
+}
